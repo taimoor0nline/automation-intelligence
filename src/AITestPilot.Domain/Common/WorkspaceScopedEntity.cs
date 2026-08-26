52cbcdb9a98ce80ba7ecbdb59ed30b90d33829dd
taimoor0nline <mirza.sahaib@gmail.com>
2026-08-26 11:11:44 +0400

    Make workspace entities auditable by default

--- src/AITestPilot.Domain/Common/WorkspaceScopedEntity.cs
+++ src/AITestPilot.Domain/Common/WorkspaceScopedEntity.cs
@@ -1,28 +1,20 @@
 namespace AITestPilot.Domain.Common;
 
-public abstract class WorkspaceScopedEntity
+public abstract class WorkspaceScopedEntity : AuditableEntity
 {
-    public Guid Id { get; protected set; }
     public Guid WorkspaceId { get; protected set; }
-    public DateTimeOffset CreatedAtUtc { get; protected set; }
-    public DateTimeOffset UpdatedAtUtc { get; protected set; }
 
-    protected WorkspaceScopedEntity(Guid workspaceId)
+    protected WorkspaceScopedEntity(Guid workspaceId) : base()
     {
         if (workspaceId == Guid.Empty)
         {
             throw new ArgumentException("WorkspaceId is required.", nameof(workspaceId));
         }
 
-        Id = Guid.NewGuid();
         WorkspaceId = workspaceId;
-        CreatedAtUtc = DateTimeOffset.UtcNow;
-        UpdatedAtUtc = CreatedAtUtc;
     }
 
     protected WorkspaceScopedEntity()
     {
     }
-
-    protected void Touch() => UpdatedAtUtc = DateTimeOffset.UtcNow;
 }
