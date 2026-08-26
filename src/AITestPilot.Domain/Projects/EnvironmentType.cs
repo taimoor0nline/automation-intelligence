ec072b5a3a7f6139ed8ad167f953a887d25f5f79
taimoor0nline <mirza.sahaib@gmail.com>
2026-08-26 10:50:38 +0400

    feat: add project environment type enum

---
+++ src/AITestPilot.Domain/Projects/EnvironmentType.cs
@@ -0,0 +1,12 @@
+namespace AITestPilot.Domain.Projects;
+
+public enum EnvironmentType
+{
+    Development = 1,
+    QualityAssurance = 2,
+    SystemIntegration = 3,
+    UserAcceptance = 4,
+    Staging = 5,
+    Production = 6,
+    Custom = 99
+}
