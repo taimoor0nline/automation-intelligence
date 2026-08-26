578eab9db3d36939d71c4b608e9dcedc7e5301ea
taimoor0nline <mirza.sahaib@gmail.com>
2026-08-26 10:52:10 +0400

    feat: add project repository abstraction

---
+++ src/AITestPilot.Application/Abstractions/IProjectRepository.cs
@@ -0,0 +1,10 @@
+using AITestPilot.Domain.Projects;
+
+namespace AITestPilot.Application.Abstractions;
+
+public interface IProjectRepository
+{
+    Task<Project?> GetByIdAsync(Guid workspaceId, Guid projectId, CancellationToken cancellationToken);
+    Task<bool> CodeExistsAsync(Guid workspaceId, string code, CancellationToken cancellationToken);
+    Task AddAsync(Project project, CancellationToken cancellationToken);
+}
