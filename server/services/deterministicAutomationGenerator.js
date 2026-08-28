0fa0b638ea007bdedc7b6d851093eb10e84b8a0e
taimoor0nline <mirza.sahaib@gmail.com>
2026-08-29 01:21:45 +0400

    Remove approved suite label from Cypress results

--- server/services/deterministicAutomationGenerator.js
+++ server/services/deterministicAutomationGenerator.js
@@ -1,1 +1,14 @@
-module.exports = require("./deterministicAutomationGeneratorV3");
+const v3 = require("./deterministicAutomationGeneratorV3");
+
+function generateDeterministicAutomation(approvedTestCases = []) {
+  const generated = v3.generateDeterministicAutomation(approvedTestCases);
+  return {
+    ...generated,
+    script: String(generated.script || "").replace(
+      "describe('AI TestPilot Approved Test Suite', () => {",
+      "describe('Test execution', () => {"
+    ),
+  };
+}
+
+module.exports = { ...v3, generateDeterministicAutomation };
